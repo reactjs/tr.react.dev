e1f74359c8769eb18f91f34850fd4b1b4a30bd40
Ali Oğuzhan Yıldız <aoguzhanyildiz@gmail.com>
2021-06-14 18:22:11 +0300

    Update MarkdownPage.js

--- src/components/MarkdownPage/MarkdownPage.js
+++ src/components/MarkdownPage/MarkdownPage.js
@@ -97,12 +97,8 @@ const MarkdownPage = ({
                 <div css={{marginTop: 15}}>
                   {date}{' '}
                   {hasAuthors && (
-<<<<<<< HEAD
-                    <span>
-=======
                     <span css={{lineHeight: 1.75}}>
                       by{' '}
->>>>>>> f3baa6d075c8de475b688abf035d7054bc8a9606
                       {toCommaSeparatedList(authors, author => (
                         <a
                           css={sharedStyles.link}
