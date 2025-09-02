7b316757b051ba1b344f15c90988dd07e3be7be5
anilcanboga <anlcn.boga@hotmail.com>
2025-09-03 01:09:10 +0300

    sync-2774ddfa

--- src/components/MDX/Sandpack/ReloadButton.tsx
+++ src/components/MDX/Sandpack/ReloadButton.tsx
@@ -12,18 +12,11 @@ export function ReloadButton({onReload}: ReloadButtonProps) {
   return (
     <button
       className="text-sm text-primary dark:text-primary-dark inline-flex items-center hover:text-link duration-100 ease-in transition mx-1"
-<<<<<<< HEAD:src/components/MDX/Sandpack/ResetButton.tsx
-      onClick={onReset}
-      title="Sandbox'ı Sıfırla"
-      type="button">
-      <IconRestart className="inline mx-1 relative" /> Sıfırla
-=======
       onClick={onReload}
       title="Keep your edits and reload sandbox"
       type="button">
       <IconRestart className="inline mx-1 relative" />
       <span className="hidden md:block">Reload</span>
->>>>>>> 2774ddfa0c39b8c2f0563b987dcb90a01ee723cf:src/components/MDX/Sandpack/ReloadButton.tsx
     </button>
   );
 }
